f669cc85e97b0f0e7d1cf2f5b79713f874eee1f2
Sag05 <34776708+Sag05@users.noreply.github.com>
2023-09-04 20:51:57 +0200

    Continued Physics

--- Assets/Scripts/Vehicles/Aircraft.cs
+++ Assets/Scripts/Vehicles/Aircraft.cs
@@ -1,64 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Aircraft : MonoBehaviour
 {
-
-    // Start is called before the first frame update
     Rigidbody rigidbody;
     float thrust;
-    Vector3 position;
-
-    float pitchAxis;
-    public float maxPitch;
-    public float minPitch;
+    public float thrustMultiplyer;
 
     float rollAxis;
+    public float rollMultiplyer;
     public float maxRoll;
     public float minRoll;
 
     float yawAxis;
+    public float yawMultiplyer;
     public float maxYaw;
     public float minYaw;
 
+    float pitchAxis;
+    public float pitchMultiplyer;
+    public float maxPitch;
+    public float minPitch;
 
-    public TextMeshProUGUI speedIndicator;
+    float lift = 0;
+    float gravity = 9.8f;
+
+    public Slider throttle;
+    
 
     void Start()
     {
-
         rigidbody = gameObject.GetComponent<Rigidbody>();
-        position = gameObject.transform.position;
+
     }
 
-    // Update is called once per frame
     void Update()
-    {
-
-
+    { 
+        //Roll
+        rollAxis = Mathf.Clamp(rollMultiplyer * Input.GetAxis("Roll"), minRoll, maxRoll) * Time.deltaTime;
+        rigidbody.AddTorque(transform.forward * rollAxis);
+        
+        //Yaw
+        yawAxis = Mathf.Clamp(yawMultiplyer * Input.GetAxis("Yaw"), minYaw, maxYaw) * Time.deltaTime;
+        rigidbody.AddTorque(transform.up *  yawAxis);
 
         //Pitch
-        pitchAxis += 100 * Mathf.Clamp(Input.GetAxis("Pitch"), minPitch, maxPitch) * Time.deltaTime;
-        //pitchAxis *= Time.deltaTime;
-
-        //Roll
-        rollAxis += 100 * Mathf.Clamp(Input.GetAxis("Roll"), minRoll, maxRoll) * Time.deltaTime;
-        //rollAxis *=Time.deltaTime;
-
-        //Yaw
-        yawAxis += 100 * Mathf.Clamp(Input.GetAxis("Yaw"), minYaw, maxYaw) * Time.deltaTime;
-        //yawAxis = Time.deltaTime;
-
-        //Rotate
-        Quaternion rotation = Quaternion.Euler(rollAxis, yawAxis, pitchAxis);
-        gameObject.transform.localRotation = rotation;
+        pitchAxis = Mathf.Clamp(pitchMultiplyer * Input.GetAxis("Pitch"), minPitch, maxPitch) * Time.deltaTime;
+        rigidbody.AddTorque(transform.right * pitchAxis);
 
 
-        thrust = position.y * Time.deltaTime;
+        //Thrust
+        thrust = Mathf.Clamp(thrust + Input.GetAxis("Thrust"), 0f, 100f);
+        Debug.Log(thrust);
+        throttle.value = thrust;
+        rigidbody.AddForce(transform.forward * thrust * thrustMultiplyer * Time.deltaTime);
 
-        //rigidbody.AddForce(transform.forward * thrust);
+
+        //Gravity
+        rigidbody.AddForce(Vector3.down * gravity);
+
+        //Lift
+        lift = gravity;
+        rigidbody.AddForce(transform.up * lift);
+
+
     }
 
 }
